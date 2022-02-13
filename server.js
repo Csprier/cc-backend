785db09d09cc759f3f84313cb11c39032ffdcaa7
Csprier <csprier@gmail.com>
2022-02-13 10:28:41 -0500

    Beginning of router building

--- server.js
+++ server.js
@@ -1,6 +1,31 @@
 const express = require('express');
+const cors = require('cors');
+
+// dotenv
+const dotenv = require('dotenv');
+dotenv.config();
+
 const app = express();
-const port = 8080;
-app.listen(port, function(){
-  console.log('The server is running on ' + port + '.');
+const PORT = 8080;
+app.use(express.json());
+app.use(cors());
+app.options('*', cors());
+
+// ==========================================
+// Routers =========================
+
+
+// Error Handler
+app.use((err, res) => {
+  res.status(err.status);
+  res.json({
+    message: err.message,
+    error: app.get('env') === 'development' ? err : err
+  });
+	console.log({ message: err.message });
+});
+
+// Run the server
+app.listen(PORT, () => {
+  console.log('The server is running on ' + PORT + '.');
 });
